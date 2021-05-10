813d08badc778ed66705ea6e165156e8c0dd4d53
Sugandhi27 <68752189+Sugandhi27@users.noreply.github.com>
2021-05-10 20:06:26 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program2Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i, j, flag;
+            Console.WriteLine("Enter minimum value");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter maximum value");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nPrime numbers between " + "{0} and {1} are: ", a, b);
+            for (i = a; i <= b; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
