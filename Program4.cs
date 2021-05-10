64366f33e859842d51c11a38eb8c0668d5213e12
Sugandhi27 <68752189+Sugandhi27@users.noreply.github.com>
2021-05-10 20:55:37 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Question4Project
+{
+    class Question4
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers:");
+            var List = new List<int>();
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                if (number > 0)
+                    List.Add(number);
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+
+            List.Sort();
+            foreach (var item in List)
+            {
+                Console.WriteLine("The numbers after sorting are:"+item);
+            }
+        }
+    }
+}
