00ec07a8d6b41fec7e8901bcfb9b71d30fb09267
Sugandhi27 <68752189+Sugandhi27@users.noreply.github.com>
2021-05-10 21:05:34 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question5Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+            int ctr = 0;
+            Console.WriteLine("Check username and password\n");
+
+            do
+            {
+                Console.WriteLine("Please Enter the username: ");
+                username = Console.ReadLine();
+
+                Console.WriteLine("Please Enter the password: ");
+                password = Console.ReadLine();
+
+                if (username != "Admin" || password != "admin")
+                {
+                    Console.WriteLine("Incorrect username or password.Try again");
+                    ctr++;
+                }
+                else
+                    Console.WriteLine("Welcome");
+                ctr = 1;
+            }
+            while ((username != "Admin" || password != "admin") && (ctr != 3));
+
+            if (ctr == 3)
+                Console.WriteLine("\nLogin attempt three or more times. Try later!");
+            else
+                Console.WriteLine("\nThe password entered successfully!");
+        }
+    }
+}
