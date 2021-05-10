6db3f5afbcce8b99827c532eb475344570be8162
Sugandhi27 <68752189+Sugandhi27@users.noreply.github.com>
2021-05-10 20:39:57 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program3Project
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<int> number = new List<int>();
+        Console.WriteLine("Enter the numbers, if you want to quit enter a negative number");
+            while (true)
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                if (i< 0)
+                    break;
+                else
+                    number.Add(i);
+            }
+    IEnumerable<int> result = number.GroupBy(r => r).Where(r => r.Count() > 1).Select(r => r.Key);
+    Console.WriteLine("The Repeated Numbers are:");
+            foreach (var solution in result)
+            {
+                Console.WriteLine(solution);
+            }
+        }
+    }
+}
